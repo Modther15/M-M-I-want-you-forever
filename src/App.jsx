58ff6d1dc192954af3bc4d7f30c35123e39159ad
Modther15 <111451646+Modther15@users.noreply.github.com>
2026-02-14 00:50:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -393,7 +393,7 @@ const Footer = () => {
   return (
     <a
       className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/UjjwalSaini07"
+      href="https://www.instagram.com/modther15?igsh=d2tuMzN4OG5tMzE2"
       target="_blank"
       rel="noopener noreferrer"
     >
@@ -401,7 +401,7 @@ const Footer = () => {
       <span role="img" aria-label="heart">
         ❤️
       </span>
-      {" "}by Ujjwal
+      {" "}by Modther
     </a>
   );
 };
